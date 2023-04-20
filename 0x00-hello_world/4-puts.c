5302a8dd713b97c5bbc30fb1e37ac1ed94cb0626
Happy -Sefah <happisos@gmail.com>
2023-04-20 15:30:37 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+  *main - Using puts function to print
+  *Return: 0 (Success)
+  */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
