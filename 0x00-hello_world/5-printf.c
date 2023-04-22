d11fa1b699f0ab80abfaec43feb1e3b257e91a05
Happy -Sefah <happisos@gmail.com>
2023-04-22 14:34:03 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
-*main-function printf 
-*return: 0 (success)
+*main - print the function printf
+*Return: 0 (Success)
 */
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
