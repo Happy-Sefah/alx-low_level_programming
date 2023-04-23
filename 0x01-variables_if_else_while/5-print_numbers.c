da82dc9f863b09ac22291fdd8bba379cf7b082f2
Happy -Sefah <happisos@gmail.com>
2023-04-23 04:04:12 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,13 +8,8 @@
 int main(void)
 {
 	int a;
-
-	for (a = 0; a < 10; a++)
-	{
-		if (a < 10)
-		{
-			printf("%d\n", a);
-		}
+	for (a = 0; a < 10; a++) {
+		printf("%d", a);
 	}
 	printf("\n");
 	return (0);
